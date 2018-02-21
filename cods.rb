cd71f0b5b7108afaac73da689b0370fc8e27bc78
Zach Gulde <zachgulde@gmail.com>
2018-02-21 10:32:02 -0600

    initial

---
+++ cods.rb
@@ -0,0 +1,13 @@
+class Cods < Formula
+  desc 'COdeup Deployment Scripts'
+  version '0.1.0'
+  url 'https://github.com/zgulde/cods.git'
+  head 'https://github.com/zgulde/cods.git', :branch => 'develop'
+
+  def install
+    bin.install 'bin/init.sh' => 'cods'
+    prefix.install 'server'
+    prefix.install 'scripts'
+    prefix.install 'templates'
+  end
+end
