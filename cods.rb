cad21d757f9dd7a443905569b205a1e30320bbd4
Zach Gulde <zachgulde@gmail.com>
2018-02-22 11:50:42 -0600

    Use tgz from github release

--- cods.rb
+++ cods.rb
@@ -1,7 +1,8 @@
 class Cods < Formula
   desc 'COdeup Deployment Scripts'
   version '0.2.0'
-  url 'https://github.com/zgulde/cods.git'
+  url 'https://github.com/zgulde/cods/archive/v0.2.tar.gz'
+  sha256 'eb84473d15341569b1f418efbefa2151b196514043f1a2329d239f3c2a45534c'
   head 'https://github.com/zgulde/cods.git', :branch => 'develop'
   devel do
     url 'https://github.com/zgulde/cods/archive/master.zip'
