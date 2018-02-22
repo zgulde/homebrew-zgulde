7ba641d7337c7d7ee96454c93d1f2099606678e7
Zach Gulde <zachgulde@gmail.com>
2018-02-22 11:14:44 -0600

    Add devel install option

--- cods.rb
+++ cods.rb
@@ -1,8 +1,12 @@
 class Cods < Formula
   desc 'COdeup Deployment Scripts'
-  version '0.1.1'
+  version '0.2.0'
   url 'https://github.com/zgulde/cods.git'
   head 'https://github.com/zgulde/cods.git', :branch => 'develop'
+  devel do
+    url 'https://github.com/zgulde/cods/archive/master.zip'
+    version '0.3.0-beta'
+  end
 
   def install
     bin.install 'bin/init.sh' => 'cods'
