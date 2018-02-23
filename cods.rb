83073b3119e149cda7f83b9c7d060ce81e38dd0b
Zach Gulde <zachgulde@gmail.com>
2018-02-23 11:14:17 -0600

    Bump version

--- cods.rb
+++ cods.rb
@@ -1,8 +1,9 @@
 class Cods < Formula
   desc 'COdeup Deployment Scripts'
-  version '0.2.0'
-  url 'https://github.com/zgulde/cods/archive/v0.2.tar.gz'
-  sha256 'eb84473d15341569b1f418efbefa2151b196514043f1a2329d239f3c2a45534c'
+  version '0.2.1'
+  url 'https://github.com/zgulde/cods/archive/v0.2.1.tar.gz'
+  sha256 'bb02bf036631df6244a79fc7d43548ea6494c02e774c676dbaa75297a71f4e02'
+
   head 'https://github.com/zgulde/cods.git', :branch => 'develop'
   devel do
     url 'https://github.com/zgulde/cods/archive/master.zip'
