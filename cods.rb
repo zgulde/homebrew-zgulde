daec73a73ae12c07f0e8e90a770d911a97c12b0e
Zach Gulde <zachgulde@gmail.com>
2019-01-24 13:44:35 -0600

    Remove development version
    it was not being used for anything and was way behind
    the most recent release

--- cods.rb
+++ cods.rb
@@ -4,11 +4,7 @@ class Cods < Formula
   url 'https://github.com/zgulde/cods/archive/v2.2.0.tar.gz'
   sha256 '292d8255377aa173b4913c94df172c2f425cb2c0fa5b1faa29e88cd8d7d01e9a'
 
-  head 'https://github.com/zgulde/cods.git', :branch => 'develop'
-  devel do
-    url 'https://github.com/zgulde/cods/archive/master.zip'
-    version '1.3.0-beta'
-  end
+  head 'https://github.com/zgulde/cods.git', :branch => 'master'
 
   def install
     bin.install 'bin/cods.sh' => 'cods'
