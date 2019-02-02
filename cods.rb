fe7375262ddbacce00cbee1747c6a7ab862bd5a1
Zach Gulde <zachgulde@gmail.com>
2019-02-01 19:30:03 -0600

    Release cods v2.2.2

--- cods.rb
+++ cods.rb
@@ -1,8 +1,8 @@
 class Cods < Formula
   desc 'COdeup Deployment Scripts'
-  version '2.2.1'
-  url 'https://github.com/zgulde/cods/archive/v2.2.1.tar.gz'
-  sha256 '355c3df76b6e04d65127daabd43f30655f1721667129dd99199d68d77ec31fa3'
+  version '2.2.2'
+  url 'https://github.com/zgulde/cods/archive/v2.2.2.tar.gz'
+  sha256 '1a057a06462aaed2f02c56b4813243ba87b91f22e2ab225630f46e4cb905abc3'
 
   head 'https://github.com/zgulde/cods.git', :branch => 'master'
 
