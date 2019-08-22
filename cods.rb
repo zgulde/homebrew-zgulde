3882d6171ead20ef6cccaef2aaf87478099cb957
Zach Gulde <zachgulde@gmail.com>
2019-08-22 09:10:56 -0500

    Release cods v3.0.0

--- cods.rb
+++ cods.rb
@@ -1,8 +1,8 @@
 class Cods < Formula
   desc 'COdeup Deployment Scripts'
-  version '2.2.3'
-  url 'https://github.com/zgulde/cods/archive/v2.2.3.tar.gz'
-  sha256 'aed1ab7ad7607d63903176c71e52f17e9a512ff8e45b5aa45bccdca1eab8243d'
+  version '3.0.0'
+  url 'https://github.com/zgulde/cods/archive/v3.0.0.tar.gz'
+  sha256 'a09f1f7f5d86d041ee278362a6b22ce9702faae0155244d292e05dbf8cb5447c'
 
   depends_on 'python'
   depends_on 'perl'
